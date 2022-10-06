c42912f5f334ff01fea4ad686c9ece121a8cc263
HinaChoudhry <61688489+HinaChoudhry@users.noreply.github.com>
2022-10-06 10:36:31 +0100

    Added documentation url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,6 +11,8 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    // documentation: https://dictionaryapi.dev/
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
