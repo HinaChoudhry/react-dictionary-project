b8be285d098742cd3e88ea70963635f2472481b1
HinaChoudhry <61688489+HinaChoudhry@users.noreply.github.com>
2022-10-07 10:03:23 +0100

    Added Phonetics component and code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,7 +8,6 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0].meanings[0].definitions[0].definition);
     setResults(response.data[0]);
   }
   function search(event) {
