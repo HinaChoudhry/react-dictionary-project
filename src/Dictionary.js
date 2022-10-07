66e52444099bc39c467df5eb0fadddc95d8b2e0d
HinaChoudhry <61688489+HinaChoudhry@users.noreply.github.com>
2022-10-07 12:16:19 +0100

    Added photos component and functionality

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
   }
+
+  let pexelsApiKey = "563492ad6f9170000100000198db4c23d5f44b54be6f4f6f775e814d";
+
+  let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+  let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+  axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -24,7 +36,7 @@ export default function Dictionary(props) {
     // documentation: https://dictionaryapi.dev/
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
   }
 
   function handleKeywordChange(event) {
@@ -52,6 +64,7 @@ export default function Dictionary(props) {
           <div className="hint">Suggested words: sunset, yoga, forest</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
